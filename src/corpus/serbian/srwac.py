cf9becb21093946c4c1c6a97c32255fc99c6d504
Ryan Echols <rkechols21@gmail.com>
2021-10-22 19:14:25 -0600

    finished srWaC loader

--- src/corpus/serbian/srwac.py
+++ src/corpus/serbian/srwac.py
@@ -1,8 +1,13 @@
 import os
+import re
 import sys
 
 
 EXPECTED_FILES = {f"srWaC1.1.0{i}.xml" for i in range(1, 7)}
+
+SENTENCE_OPEN_RE = re.compile("<s>")
+SENTENCE_CLOSE_RE = re.compile("</s>")
+TOKEN_RE = re.compile(r"(\S+)(?:\s+\S+){3}")
 
 
 class SrWaC:
@@ -15,11 +20,26 @@ class SrWaC:
         self.file_paths.sort()
         self.n = 0
         for file_path in self.file_paths:
-            pass  # TODO: iterate and increment n
+            with open(file_path, "r", encoding="utf-8") as file:
+                for line in file:
+                    self.n += len(SENTENCE_OPEN_RE.findall(line))
 
     def __len__(self) -> int:
         return self.n
 
     def __iter__(self):
         for file_path in self.file_paths:
-            pass  # TODO: iterate and yield
+            with open(file_path, "r", encoding="utf-8") as file:
+                in_sentence = False
+                sentence = list()
+                for line in file:
+                    if SENTENCE_OPEN_RE.search(line):
+                        in_sentence = True
+                        sentence = list()
+                    elif SENTENCE_CLOSE_RE.search(line):
+                        in_sentence = False
+                        yield sentence
+                    elif in_sentence:
+                        token_match = TOKEN_RE.search(line)
+                        if token_match is not None:
+                            sentence.append(token_match.group(1))
