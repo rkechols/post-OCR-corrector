3b26552e38143500c613a2b2a2f847a3287d6616
Ryan Echols <rkechols21@gmail.com>
2021-10-22 22:49:38 -0600

    added recognition of the 'no-space' token/tag ('<g/>')

--- src/corpus/serbian/srwac.py
+++ src/corpus/serbian/srwac.py
@@ -7,6 +7,8 @@ EXPECTED_FILES = {f"srWaC1.1.0{i}.xml" for i in range(1, 7)}
 
 SENTENCE_OPEN_RE = re.compile("<s>")
 SENTENCE_CLOSE_RE = re.compile("</s>")
+NO_SPACE_TAG = "<g/>"
+NO_SPACE_RE = re.compile(NO_SPACE_TAG)
 TOKEN_RE = re.compile(r"(\S+)(?:\s+\S+){3}")
 
 
@@ -15,7 +17,7 @@ class SrWaC:
         self.dir_path = dir_path
         found_files = os.listdir(dir_path)
         if set(found_files) != EXPECTED_FILES:
-            print("WARNING: the files found do not match the expected files", file=sys.stderr)
+            print("WARNING: the file names found do not match the expected file names", file=sys.stderr)
         self.file_paths = [os.path.join(self.dir_path, file_name) for file_name in found_files]
         self.file_paths.sort()
 
@@ -24,14 +26,25 @@ class SrWaC:
             with open(file_path, "r", encoding="utf-8") as file:
                 in_sentence = False
                 sentence = list()
-                for line in file:
-                    if SENTENCE_OPEN_RE.search(line):
+                for line_ in file:
+                    line = line_.strip()
+                    if SENTENCE_OPEN_RE.fullmatch(line):
+                        if in_sentence:
+                            print("WARNING: nested sentence?", file=sys.stderr)
                         in_sentence = True
                         sentence = list()
-                    elif SENTENCE_CLOSE_RE.search(line):
+                    elif SENTENCE_CLOSE_RE.fullmatch(line):
+                        if not in_sentence:
+                            print("WARNING: sentence close without open?", file=sys.stderr)
                         in_sentence = False
                         yield sentence
                     elif in_sentence:
-                        token_match = TOKEN_RE.search(line)
-                        if token_match is not None:
-                            sentence.append(token_match.group(1))
+                        if NO_SPACE_RE.fullmatch(line):
+                            sentence.append(NO_SPACE_TAG)
+                        else:
+                            token_match = TOKEN_RE.fullmatch(line)
+                            if token_match is not None:
+                                sentence.append(token_match.group(1))
+                            else:
+                                print("WARNING: unexpected line within a sentence", file=sys.stderr)
+                                print(line, file=sys.stderr)
--- src/corpus/serbian/to_plain_text.py
+++ src/corpus/serbian/to_plain_text.py
@@ -1,13 +1,29 @@
 import argparse
 import os
+from typing import List
 
 from tqdm import tqdm
 
 from corpus.serbian import CORPUS_DIR, DEFAULT_ENCODING
-from corpus.serbian.srwac import SrWaC
+from corpus.serbian.srwac import NO_SPACE_TAG, SrWaC
 
 
 CORPUS_PLAIN_FILE_PATH = os.path.join(CORPUS_DIR, "srWaC-plain.txt")
+
+
+def sentence_to_string(tokens: List[str]) -> str:
+    to_return = list()
+    saw_joiner = False
+    for token in tokens:
+        if token == NO_SPACE_TAG:
+            saw_joiner = True
+            continue
+        if saw_joiner:
+            saw_joiner = False
+            to_return[-1] += token
+        else:
+            to_return.append(token)
+    return " ".join(to_return)
 
 
 if __name__ == "__main__":
@@ -19,8 +35,12 @@ if __name__ == "__main__":
     corpus = SrWaC(args.srwac_path)
 
     print(f"Converting corpus to plain text...")
+    longest = 0
     with open(CORPUS_PLAIN_FILE_PATH, "w", encoding=DEFAULT_ENCODING) as out_file:
         for sentence in tqdm(corpus):
-            print(" ".join(sentence), file=out_file)
+            sentence_str = sentence_to_string(sentence)
+            longest = max(longest, len(sentence_str))
+            print(sentence_str, file=out_file)
 
     print(f"Plain text corpus saved to {CORPUS_PLAIN_FILE_PATH}")
+    print(f"Longest sentence was {longest} characters.")
