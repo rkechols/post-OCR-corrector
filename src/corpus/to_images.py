d79cdef0667a990899b55d5cb4084d7d05789fb7
Ryan Echols <rkechols21@gmail.com>
2021-10-26 15:49:23 -0600

    figured out that storing all images on disk is not feasible

--- src/corpus/to_images.py
+++ src/corpus/to_images.py
@@ -1,16 +1,10 @@
-import argparse
-import multiprocessing as mp
-import os
 import random
-from typing import Tuple
 
 import numpy as np
 import PIL
 from PIL import Image, ImageDraw, ImageFont
 from skimage.util import random_noise
-from tqdm import tqdm
 
-from corpus import DEFAULT_ENCODING
 from util.fonts import ALL_FONTS
 
 
@@ -22,23 +16,7 @@ MARGIN_HORIZONTAL = 16
 MARGIN_VERTICAL = 16
 
 
-# make sure the images dir exists, but is empty
-IMAGES_DIR = os.path.join("data", "corpus", "images")
-os.makedirs(IMAGES_DIR, exist_ok=True)
-for file_name_ in os.listdir(IMAGES_DIR):
-    file_path_ = os.path.join(IMAGES_DIR, file_name_)
-    if os.path.isfile(file_path_):
-        os.remove(file_path_)
-
-
-def number_to_filename(i: int) -> str:
-    # formats i to have 8 digits by adding leading zeroes if needed
-    # (8 digits because there are 25,636,542 sentences)
-    return f"sentence_{i:08d}.png"
-
-
-def sentence_to_image(index_sentence: Tuple[int, str]):
-    index, sentence = index_sentence
+def sentence_to_image(sentence: str) -> Image.Image:
     # make a canvas
     image = Image.new("RGB", (IMAGE_WIDTH, IMAGE_HEIGHT), color=HEX_WHITE)
     draw = ImageDraw.Draw(image)
@@ -73,34 +51,4 @@ def sentence_to_image(index_sentence: Tuple[int, str]):
     np_image = (255 * np_image).astype(np.uint8)
     image = Image.fromarray(np_image)
     # save the image to disk
-    image.save(os.path.join(IMAGES_DIR, number_to_filename(index)))
-
-
-if __name__ == "__main__":
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("plain_text_path", type=str, help="File path to the plain text file to create images from.")
-    arg_parser.add_argument("--n-total", type=int, default=25636542, help="Number of sentences being turned into images.")
-    arg_parser.add_argument("--cpu-limit", type=int, default=(os.cpu_count() - 1), help="Maximum number of CPU cores allowed to use.")
-    args = arg_parser.parse_args()
-    plain_text_path = args.plain_text_path
-    n_total = args.n_total
-    cpu_limit = args.cpu_limit
-
-    cpus_available = max(min(os.cpu_count(), cpu_limit), 1)  # the outer max() is in case we get a non-positive number for the CPU limit
-    chunk_size = n_total // (cpus_available * 8)
-
-    print(f"Converting lines of {plain_text_path} to images...")
-
-    with open(plain_text_path, "r", encoding=DEFAULT_ENCODING) as corpus_file:
-        sentence_generator = ((i_, line.strip()) for i_, line in enumerate(corpus_file, start=1) if line.strip() != "")
-        if cpus_available <= 2:  # multiprocessing isn't worth it
-            print("Using 1 CPU core.")
-            for tup in tqdm(sentence_generator, total=n_total):
-                sentence_to_image(tup)
-        else:  # cpus_available >= 3
-            print(f"Using {cpus_available} CPU cores (chunk size = {chunk_size}).")
-            with mp.Pool(cpus_available) as pool:
-                for _ in tqdm(pool.imap_unordered(sentence_to_image, sentence_generator, chunksize=chunk_size), total=n_total):
-                    pass
-
-    print(f"Created images of text in directory {IMAGES_DIR}")
+    return image
