8172f39b5d7b1cf257a804d7d9987105d69cb9d5
Ryan Echols <rkechols21@gmail.com>
2021-10-29 22:02:39 -0600

    finished first draft of string mutilating function

--- src/corpus/corrector_dataset.py
+++ src/corpus/corrector_dataset.py
@@ -1,6 +1,7 @@
 import random
 import re
 import time
+from enum import auto, Enum
 from typing import Tuple
 
 import pandas as pd
@@ -11,15 +12,23 @@ from corpus import DEFAULT_ENCODING
 
 WHITESPACE_RE = re.compile(r"\s+")
 
+EDIT_CHANCE = 0.15
 
-def mutilate_string(text: str) -> str:
-    pass
+
+class EditType(Enum):
+    DELETE = auto()
+    CHANGE = auto()
+    INSERT = auto()
+
+
+N_EDIT_TYPES = len(EditType) + 1
 
 
 class CorrectorDataset(Dataset):
-    def __init__(self, split_csv_path: str, plain_txt_path: str):
+    def __init__(self, split_csv_path: str, plain_txt_path: str, good_chars: str):
         self.split_frame = pd.read_csv(split_csv_path)
         self.plain_txt_path = plain_txt_path
+        self.good_chars = good_chars
 
     def __len__(self) -> int:
         return len(self.split_frame)
@@ -35,29 +44,75 @@ class CorrectorDataset(Dataset):
                     break
                 chars.append(char)
         text_correct = "".join(chars)
-        text_messy = mutilate_string(text_correct)
+        text_messy = self._mutilate_string(text_correct)
         return text_messy, text_correct
+
+    def _mutilate_string(self, text: str) -> str:
+        n = len(text)
+        to_return = list()
+        i = 0
+        while i < n:
+            if random.uniform(0.0, 1.0) <= EDIT_CHANCE:  # yes edit
+                if random.randrange(0, N_EDIT_TYPES) == 0:
+                    pass
+                else:
+                    for _ in range(random.choice([1, 2])):  # edit 1 or 2 chars right here
+                        edit_type = random.choice(list(EditType))
+                        if edit_type == EditType.DELETE:
+                            # just increment the index without copying characters
+                            i += 1
+                        elif edit_type == EditType.CHANGE:
+                            # pick a replacement character
+                            new_char = random.choice(self.good_chars)
+                            to_return.append(new_char)
+                            i += 1  # move past the real one
+                        elif edit_type == EditType.INSERT:
+                            # pick a character to add
+                            new_char = random.choice(self.good_chars)
+                            to_return.append(new_char)
+                            # don't increment `i` so the real char will actually be added
+                        # else: UNKNOWN
+            else:  # no edit
+                to_return.append(text[i])
+                i += 1
+        # we could also insert at the end
+        if random.uniform(0.0, 1.0) <= EDIT_CHANCE:
+            for _ in range(random.choice([1, 2])):  # edit 1 or 2 chars right here
+                # pick a character to add
+                new_char = random.choice(self.good_chars)
+                to_return.append(new_char)
+        # return the new version of the string
+        return "".join(to_return)
 
 
 if __name__ == "__main__":
+    with open("data/corpus/serbian/good_chars.txt", "r", encoding=DEFAULT_ENCODING) as good_chars_file:
+        good_chars_ = good_chars_file.read()
+    good_chars_ = good_chars_.replace("\n", "")  # \n is never a "good char", but it may be in the file if they put it on multiple lines
+
     time_start = time.time()
-    dataset = CorrectorDataset("data/corpus/serbian/split.csv", "data/corpus/serbian/srWaC-plain.txt")
+    dataset = CorrectorDataset("data/corpus/serbian/split.csv", "data/corpus/serbian/srWaC-plain.txt", good_chars_)
     time_end = time.time()
+
     time_diff = time_end - time_start
     print(f"TIME TO LOAD: {time_diff:.2f} seconds")
     n_total = len(dataset)
     print("SIZE:", n_total)
+
     same_count = 0
-    time_start = time.time()
     n_tests = 100
-    for i in range(n_tests):
-        messy, correct = dataset[i]
+
+    time_start = time.time()
+    for i_ in range(n_tests):
+        messy, correct = dataset[i_]
         if messy == correct:
             same_count += 1
     time_end = time.time()
+
+    print("SAME COUNT:", same_count, f"(of {n_tests})")
+
     time_diff = time_end - time_start
     time_per_item = time_diff / n_tests
-    print("SAME COUNT:", same_count)
     print(f"AVG TIME TO LOAD EACH ITEM: {time_per_item:.4f} seconds")
     expected_total = time_per_item * n_total
     print(f"EXPECTED TIME TO LOAD ALL: {expected_total:.2f} seconds")
